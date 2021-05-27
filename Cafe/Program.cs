b3f1c5dd1fc333001ceb1c6731df82cd3a9116c6
PedroLCamara <plcrlschool@gmail.com>
2021-05-27 15:02:59 -0300

    Estilo_atv_cafeteira

--- Cafe/Program.cs
+++ Cafe/Program.cs
@@ -8,42 +8,84 @@ namespace Cafe
         static void Main(string[] args)
         {
             cafeteira c = new cafeteira();
-            bool cafeteiraOn= true;
+            bool cafeteiraOn = true;
             string resposta;
             int acucarEspecifico;
+            int acucarInicial = c.acucarDisponivel;
             do
             {
-                Console.WriteLine($@"
-============= Super CafeteiraTabajaras Plus++ =============
-Capacidade ------- {c.acucarDisponivel}gr
-1----------------- Fazer café
-2----------------- Fazer café com açucar especificado (gr)
-3----------------- Desligar
+                if (c.acucarDisponivel > acucarInicial * 0.33)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($@"
+=============== Super CafeteiraTabajaras Plus++ ===============
+Quant. açucar ------- {c.acucarDisponivel}gr
+1-------------------- Fazer café
+2-------------------- Fazer café com açucar especificado (gr)
+3-------------------- Desligar
 ");
-                resposta= Console.ReadLine();
-                if (resposta=="1")
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else if (c.acucarDisponivel > 10)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($@"
+=============== Super CafeteiraTabajaras Plus++ ===============
+Quant. açucar ------- {c.acucarDisponivel}gr
+1-------------------- Fazer café
+2-------------------- Fazer café com açucar especificado (gr)
+3-------------------- Desligar
+");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else if(c.acucarDisponivel > 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($@"
+=============== Super CafeteiraTabajaras Plus++ ===============
+Quant. açucar ------- {c.acucarDisponivel}gr
+1-------------------- Fazer café
+2-------------------- Fazer café com açucar especificado (gr)
+3-------------------- Desligar
+");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine($@"
+=============== Super CafeteiraTabajaras Plus++ ===============
+Quant. açucar ------- {c.acucarDisponivel}gr
+1-------------------- Fazer café
+2-------------------- Fazer café com açucar especificado (gr)
+3-------------------- Desligar
+");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                resposta = Console.ReadLine();
+                if (resposta == "1")
                 {
                     c.cafeteiraUsar();
-                    cafeteiraOn= true;
-                } 
-                else if (resposta=="2")
+                    cafeteiraOn = true;
+                }
+                else if (resposta == "2")
                 {
                     Console.WriteLine("Qual a quantidade de açucar desejada?");
-                    acucarEspecifico= int.Parse(Console.ReadLine());
+                    acucarEspecifico = int.Parse(Console.ReadLine());
                     c.cafeteiraUsar(acucarEspecifico);
-                    cafeteiraOn= true;
+                    cafeteiraOn = true;
                 }
-                else if (resposta=="3")
+                else if (resposta == "3")
                 {
                     Console.WriteLine("Desligando...");
-                    cafeteiraOn= false;
+                    cafeteiraOn = false;
                 }
                 else
                 {
                     Console.WriteLine("Resposta inválida, digite '1' para fazer café, '2' para fazer café com uma quantia específica de açucar e '3' para desligar a cafeteira.");
-                    cafeteiraOn= true;
+                    cafeteiraOn = true;
                 }
-            } while (cafeteiraOn== true);
+            } while (cafeteiraOn == true);
         }
     }
 }
